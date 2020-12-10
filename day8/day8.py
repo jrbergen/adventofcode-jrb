0ac510d3558145f83d41a17daf1ba305ec50e175
joost_bergen <j.r.bergen@enpicom.com>
2020-12-10 22:56:37 +0100

    Day 10. Minor changes day 8.

--- day8/day8.py
+++ day8/day8.py
@@ -14,9 +14,11 @@ def timing(f):
         return result
     return wrap
 
+
 def read_instructions(text_file: Union[str, Path]) -> List[List[Union[str, int]]]:
     text_file = text_file if isinstance(text_file, Path) else Path(text_file).resolve()
     return [[line.split(' ')[0], int(line.split(' ')[1])] for line in text_file.read_text().split('\n') if line]
+
 
 @timing
 def part1(program: List[List[Union[str, int]]]) -> NoReturn:
@@ -48,11 +50,10 @@ def part1(program: List[List[Union[str, int]]]) -> NoReturn:
 def bruteforce_instruction_replacement(repl_idx: List[int], replacement: str,
                                        program: List[List[str]], verbose: bool = False):
     bruteforce_attempts = 0
-    oldprogram = tuple(program)
+    program = tuple(program)
     for repl_index in repl_idx:
 
-        program = list(oldprogram)
-
+        oldval = program[repl_index][0]
         program[repl_index][0] = replacement
 
         acc, readhead = 0, 0
@@ -62,10 +63,10 @@ def bruteforce_instruction_replacement(repl_idx: List[int], replacement: str,
         while True:
 
             if readhead > len(program) - 1 or readhead < 0:
-                print(f"Found terminating state after {bruteforce_attempts} replacement attempts...")
-                print(f"Value of 'acc' upon termination was: {acc}")
-                print(f"Program terminates if '{oldprogram[repl_index]}' at line {repl_index}",
-                      " is replaced by '{replacement}'")
+                print('\n'.join([f"Found terminating state after {bruteforce_attempts} replacement attempts...",
+                                 f"Value of 'acc' upon termination was: {acc}",
+                                 f"Program terminates if '{oldval}' at line {repl_index} is ",
+                                 f"replaced by '{replacement}'"]))
                 return True
 
             if readhead in ran_instructions:
@@ -73,6 +74,7 @@ def bruteforce_instruction_replacement(repl_idx: List[int], replacement: str,
                     print("Oh no! Program entered infinite loop... Breaking.")
                     print(f"Last acc = {acc}")
                 bruteforce_attempts += 1
+                program[repl_index][0] = oldval
                 break
 
             instruction, value = program[readhead]
@@ -88,10 +90,11 @@ def bruteforce_instruction_replacement(repl_idx: List[int], replacement: str,
                 readhead += value
     return False
 
+
 @timing
 def part2(program: List[List[Union[str, int]]]) -> NoReturn:
-    nopidx = (lineno for lineno, instr in enumerate(program) if instr[0] == 'nop')
-    jmpidx = (lineno for lineno, instr in enumerate(program) if instr[0] == 'jmp')
+    nopidx = [lineno for lineno, instr in enumerate(program) if instr[0] == 'nop']
+    jmpidx = [lineno for lineno, instr in enumerate(program) if instr[0] == 'jmp']
 
     for idxlist, repl in zip((nopidx, jmpidx,), ('jmp', 'nop',)):
         if bruteforce_instruction_replacement(idxlist, replacement=repl, program=program):
